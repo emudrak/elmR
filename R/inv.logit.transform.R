374fda350aa8350542a9c7c4d7420f1fecc730fe
Erika Mudrak <erika.mudrak@gmail.com>
2014-08-28 10:40:21 -0400

    fixed documentation and added inverse logit

---
+++ R/inv.logit.transform.R
@@ -0,0 +1,20 @@
+#' Inverse Logit Transformation
+#'
+#' Perform the inverse logit transformation for data with zeros and ones included #' (Phil Dixon's suggestion)
+#' @param     #alpha is a vector of real number
+#' @keywords inverse logit tranform
+#' @return a vector of inverse logit transformed values 
+#'    if(alpha>705) return(1) #otherwise too big to exp()
+#' @export 
+#' @seealso 
+#' @examples
+#' pc=seq(0,1,by=0.01)
+#' plot(pc, logit.transform(pc), pch=19, col="gray", cex=1.2, ylab="transformed value")
+#' points(pc, log(pc/(1-pc)), pch=20)
+
+inv_logit=function(alpha){
+    #alpha is a vector of real number
+    #if(alpha>705) return(1) #otherwise too big to exp()
+    alpha[alpha>709]=709   #This is largest number R will take to 1.  Otherwise NaN
+    return(exp(alpha)/(exp(alpha)+1))
+}
--- R/logit.transform.R
+++ R/logit.transform.R
@@ -4,11 +4,15 @@
 #' (Phil Dixon's suggestion)
 #' @param x is a vector of values between 0 and 1 inclusive
 #' @keywords logit tranform
+#' @return a vector of logit transformed values
 #' @export 
+#' @seealso 
 #' @examples
-#' logit.transform(seq(0,1))
+#' pc=seq(0,1,by=0.01)
+#' plot(pc, logit.transform(pc), pch=19, col="gray", cex=1.2, ylab="transformed value")
+#' points(pc, log(pc/(1-pc)), pch=20)
 
-logit.transform = function(x){  
+logit = function(x){  
     #  x is a vector of values between 0 and 1 inclusive
     if(sum(x[x!=0], na.rm=TRUE)==0) 
         eps=0
